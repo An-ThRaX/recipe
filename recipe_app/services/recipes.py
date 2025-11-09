5ef01e883abd1a820b7d41aea81f6ca31d5d956b
ciprian <ciprianionutz93@gmail.com>
2025-11-09 12:07:59 +0100

    added a temp func to add recipes to DB

--- recipe_app/services/recipes.py
+++ recipe_app/services/recipes.py
@@ -2,7 +2,14 @@ from db import engine
 from db.models import Recipe
 from sqlmodel import Session
 
+import random
+import string
 
+def random_string(n):
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=n))
+
+
+# this will have to be used as the add btn on a formular
 def add_recipe(
     title: str,
     ingredients: str,
@@ -13,6 +20,17 @@ def add_recipe(
             title=title,
             ingredients=ingredients,
             instructions=instructions
+        )
+        session.add(recipe)
+        session.commit()
+
+# this will be used as temp btn
+def add_pseudo_recipe():
+    with Session(engine) as session:
+        recipe= Recipe(
+            title=random_string(5),
+            ingredients=random_string(10),
+            instructions=random_string(10)
         )
         session.add(recipe)
         session.commit()
